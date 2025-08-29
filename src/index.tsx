5a6a60dcafff640934098bd14d7ac7721d4bd3f4
MehrabVosough <mehrabvosough.dp@gmail.com>
2025-08-29 11:41:33 +0330

    add: index and app react file

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,9 @@
 import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./MainApp"
 
-const App: React.FC = () => {
-    const sendNotification = () => {
-        window.electronAPI.notify("React + TypeScript Notification 🚀");
-    };
-
-    return (
-        <div style={{ textAlign: "center", padding: "2rem" }}>
-            <h1>Jira Notifier (React + Electron + TS)</h1>
-            <button onClick={sendNotification}>Send Test Notification</button>
-        </div>
-    );
-};
-
-export default App;
+const container = document.getElementById("root");
+if (container) {
+    const root = createRoot(container);
+    root.render(<App />);
+}
